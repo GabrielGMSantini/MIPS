dd79882b7fe948e817c8d0f68a09ea777e7eb3f1
Luiz <53343378+luizvruoso@users.noreply.github.com>
2019-08-24 20:55:08 -0300

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -8,17 +8,25 @@ opt4:  .asciiz "4) show monthly expenses\n"
 opt5:  .asciiz "5) show expenses sorted by category\n"
 opt6:  .asciiz "6) rank the expenses\n"
 quit:  .asciiz "7) quit\n"
-tst1:  .asciiz "entrou no 1\n"
-tst2:  .asciiz "entrou na 2\n"
-tst3:  .asciiz "entrou no 3\n"
-tst4:  .asciiz "entrou no 4\n"
-tst5:  .asciiz "entrou no 5\n"
-tst6:  .asciiz "entrou no 6\n\n\n\n"
+entrouOp1:  .asciiz "entrou no 1\n"
+entrouOp2:  .asciiz "entrou na 2\n"
+entrouOp3:  .asciiz "entrou no 3\n"
+entrouOp4:  .asciiz "entrou no 4\n"
+entrouOp5:  .asciiz "entrou no 5\n"
+entrouOp6:  .asciiz "entrou no 6\n\n\n\n"
+msgOp1_1:   .asciiz "Digite o valor da em reais da despesa\n"
+idBD: .byte 4
+
+array: .word 40
+tamArray: .byte 40
+posAtualArray: .byte 4
+
 
 .text
 .globl main
 main:
-
+	
+	
 	li $v0, 4
 	la $a0, welcome
 	syscall
@@ -51,7 +59,6 @@ main:
 	syscall
 	
 	beq $v0,1,registro
-	j regitrarDespesa
 	beq $v0,2,listar
 	beq $v0,3,excluir
 	beq $v0,4,exibir_mensal
@@ -60,37 +67,56 @@ main:
 	j main
 
 registro:
-	li $v0,4
-	la $a0, tst1
+	#Msg de entrada na func
+	li $v0, 4
+	la $a0, entrouOp1
 	syscall
+	#fim da gloriosa
+	
+	li $v0, 4
+	la $a0, msgOp1_1
+	syscall
+	#lendo valor
+	
+	li $v0, 6
+	syscall
+	
+	#f0 contem o valor lido
+	la  $t0, array #carreguei em t0 o end do vetor
+	mfc1 $a0, $f0 #esta movendo do coprocessador1 o valor float para o registrador $a0
+	sw $a0, 0($t0)#gravando na pos 0
+	
+	
+	li $v0, 2
+	l.s $f12, 0($t0)
+	syscall
+	
+
+	
 	j main
 listar:
 	li $v0,4
-	la $a0, tst2
+	la $a0, entrouOp2
 	syscall
 	j main
 excluir:
 	li $v0,4
-	la $a0, tst3
+	la $a0, entrouOp3
 	syscall
 	j main
 exibir_mensal:
 	li $v0,4
-	la $a0, tst4
+	la $a0, entrouOp4
 	syscall
 	j main
 exibir_categoria:
 	li $v0,4
-	la $a0, tst5
+	la $a0, entrouOp5
 	syscall
 	j main
 exibir_rank_despesa:
 	li $v0,4
-	la $a0, tst6
+	la $a0, entrouOp6
 	syscall
 	j main
 	
-registrarDespesa:
-
-
-	j main
